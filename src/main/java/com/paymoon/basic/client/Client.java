9bb89c92f6c69dc626c28095d422aafce947ddb4
stevenlii <stevenlii@qq.com>
2017-07-05 15:47:00 +0800

    更改client为junit

--- src/main/java/com/paymoon/basic/client/Client.java
+++ src/main/java/com/paymoon/basic/client/Client.java
@@ -1,17 +1,34 @@
 package com.paymoon.basic.client;
+import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import com.paymoon.basic.service.StudentService;
 
-public class Client  {
+import junit.framework.TestCase;
 
-   public static void main(String[] args) {
-	   @SuppressWarnings("resource")
+public class Client extends TestCase {
+
+	private StudentService sService;
+    protected void setUp() throws Exception {   
+        super.setUp();   
+        @SuppressWarnings("resource")
 		BeanFactory context = new FileSystemXmlApplicationContext("/src/main/resources/app-context.xml");
-	   StudentService sService = (StudentService)context.getBean("studentService");   
-//       sService.addStudent();
-       sService.addStudentBatch();
-}
+	    sService = (StudentService)context.getBean("studentService");   
+    }  
+    
+    protected void tearDown() throws Exception {   
+        super.tearDown();   
+    }   
+    
+    //单个添加
+	@Test
+	public void testRequires(){
+		sService.addStudent();
+	}
+	@Test
+	public void testAddStudentBatch(){
+		 sService.addStudentBatch();
+	}
 	
 }
